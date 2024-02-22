779ed63c6fdae1dbe7383963d87a578e74b7a76c
Jack Huynh <huynhtrongnghia6102004@gmail.com>
2024-02-22 15:17:28 -0500

    somewhat

--- src/main/java/com/example/hw2/repository/InventoryRepository.java
+++ src/main/java/com/example/hw2/repository/InventoryRepository.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
 import com.example.hw2.model.Guitar;
 
+@Repository
 public class InventoryRepository {
     private List<Guitar> guitars = new ArrayList<>();
     
